3bac4f4eab38e6cadb793104cbd4785f89e4c422
kompotiks <47722954+kompotiks@users.noreply.github.com>
2021-02-22 13:27:14 +0300

    New

--- diarization.py
+++ diarization.py
@@ -6,6 +6,10 @@ import soundfile as sf
 import argparse
 import os
 
+from tqdm import tqdm
+from pydub import AudioSegment
+import math
+
 
 def create_parser():
     """
@@ -13,9 +17,10 @@ def create_parser():
     :return ArgumentParser representing the command line arguments that were supplied to the command line:
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--audio", help="audio fragment")
+    parser.add_argument("--audio", help="audio fragment or directory with them")
     parser.add_argument("--noise", help="audio fragment with noise")
     parser.add_argument("--voice", help="audio fragment with voice")
+    parser.add_argument("--cuts_on", help="parts duration in seconds", default=None, type=int, required=False)
     parser.add_argument("--plot", help="display image", action="store_true")
     parser.add_argument("--min_len_audio", help="minimal length split audio", default=0.2, type=float)
     parser.add_argument("--add_time", help="adding time for voice", default=0.3, type=float)
@@ -42,6 +47,7 @@ def read_file(path):
         output: data - массив значений аудио, samplerate - характеристика аудио
     """
     data, samplerate = sf.read(path)
+
     if samplerate != 16000:
         data, _ = read_file(convert_wav(path))
     return data, samplerate
@@ -74,43 +80,143 @@ def get_change_moments(similarity_dict, wav_splits, plot):
         plt.plot(data_1)
         plt.show()
     diar_frags = list(wav_splits[res[1:]])
-    choose = lambda x: x[0] + (x[1] - x[0]) / 3 # TODO
+    choose = lambda x: x[0] + (x[1] - x[0]) / 3  # TODO
     return [wav_splits[0][0]] + list(map(choose, diar_frags)) + [wav_splits[-1][1]], res_names
 
 
-def get_fragment_parts(change_moments, names):
+def get_fragment_parts(change_moments, names, ):
     res = []
     for i in range(len(names)):
         res.append([names[i], change_moments[i], change_moments[i + 1]])
     return res
 
 
-def split_wav(wav, sr, timeline, add_time, min_len_audio, dir_save):
-    for idx, (label, start, end) in enumerate(timeline):
+def split_wav(wav, sr, timeline, add_time, min_len_audio, dir_save, start_from=None):
+    voise_iterator = tqdm(timeline, ascii=True, leave=False)
+    for idx, (label, start, end) in enumerate(voise_iterator):
+        voise_iterator.set_description(f'{label} fragment {idx} processing', refresh=True)
         if min_len_audio < end - start:
             if label == 'voice':
-                sf.write(f'{dir_save}/{idx}-{label}.wav', wav[int(start * sr): int(end * sr) + int(add_time * sr)], sr)
+                if start_from:
+                    voice_file_name = f'{dir_save}/{idx + start_from}-{label}'
+                else:
+                    voice_file_name = f'{dir_save}/{idx}-{label}'
+                if os.path.exists(voice_file_name):
+                    print(f'File "{voice_file_name}" already existed!')
+                    voice_file_name = voice_file_name + '_1'
+
+                voice_file_name = f'{voice_file_name}.wav'
+
+                sf.write(voice_file_name, wav[int(start * sr): int(end * sr) + int(add_time * sr)], sr)
+
+
+def from_dir(dir):
+    '''
+    :param dir: path to directory
+    :return: list off files in directory
+    '''
+    all_files = []
+    for _, _, files in os.walk(dir):
+        for filename in files:
+            all_files.append(dir + '/' + filename)
+    return all_files
+
+
+def cut_wav_on_parts(wav, cuts_on):
+    '''
+    Cutting a big audio file into parts
+    :param wav: "audio.wav" file, which must be cutted into parts
+    :param cuts_on: parts duration in seconds (int)
+    :return: dict of new files names and info about begin and end of audio
+    '''
+    audio = AudioSegment.from_wav(wav)
+    total_secs = math.ceil(audio.duration_seconds)
+    file_names = {}
+    iterator = tqdm(range(0, total_secs, args.cuts_on), leave=False)
+    for part in iterator:
+        from_sec = part
+        to_sec = part + cuts_on if part + cuts_on < total_secs else total_secs
+        file_name = f'{from_sec}_to_{to_sec}_secs_{os.path.basename(wav)}'
+        audio[from_sec * 1000:to_sec * 1000].export(file_name, format='wav')
+        iterator.set_description(f'{file_name} is create')
+        file_names[file_name] = {'begin': from_sec, 'end': to_sec}
+
+    return file_names, total_secs
 
 
 def diarize(args):
-    if not os.path.exists(f'./{args.dir_save}/'): os.mkdir(f'./{args.dir}/')
+    if not os.path.exists(f'./{args.dir_save}/'):
+        os.mkdir(f'./{args.dir_save}/')
 
     encoder = VoiceEncoder(args.device, verbose=False)
 
-    wav, sr_0 = read_file(args.audio)
-    noise, sr_1 = read_file(args.noise)
-    voice, sr_2 = read_file(args.voice)
+    def wav_proccecing(args, piece=None, start_from=None):
+        '''
+        :param args: parameters from console
+        :param piece: If not the whole file (only piece) was given (name and path_to_dir)
+        :param start_from: for the beauty of new file naming and for correctly display the time interval
+        :return: info about postprocessing
+        '''
 
-    _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
-    noise_similarity = get_similarity(encoder, cont_embeds, noise)
-    voice_similarity = get_similarity(encoder, cont_embeds, voice)
+        if isinstance(piece, dict):
+            wav, sr_0 = read_file(piece['name'])
+        else:
+            wav, sr_0 = read_file(piece)
 
-    similarity_dict = {'noise': noise_similarity, 'voice': voice_similarity}
-    wav_splits_seconds = np.array(list(map(lambda x: [x.start / sampling_rate, x.stop / sampling_rate], wav_splits)))
+        noise, sr_1 = read_file(args.noise)
+        voice, sr_2 = read_file(args.voice)
 
-    change_moments, names = get_change_moments(similarity_dict, wav_splits_seconds, args.plot)
-    diarized_fragments = get_fragment_parts(change_moments, names)
-    split_wav(wav, sr_0, diarized_fragments, dir_save=args.dir_save, add_time=args.add_time, min_len_audio=args.min_len_audio)
+        _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
+        noise_similarity = get_similarity(encoder, cont_embeds, noise)
+        voice_similarity = get_similarity(encoder, cont_embeds, voice)
+
+        similarity_dict = {'noise': noise_similarity, 'voice': voice_similarity}
+        wav_splits_seconds = np.array(
+            list(map(lambda x: [x.start / sampling_rate, x.stop / sampling_rate], wav_splits)))
+
+        change_moments, names = get_change_moments(similarity_dict, wav_splits_seconds, args.plot)
+        if start_from:
+            change_moments = [t for t in change_moments]
+
+        diarized_fragments = get_fragment_parts(change_moments, names)
+
+        split_wav(wav, sr_0, diarized_fragments, dir_save=piece['path_to_dir'], add_time=args.add_time,
+                  min_len_audio=args.min_len_audio, start_from=start_from)
+
+        return diarized_fragments
+
+    if not os.path.isfile(args.audio):
+        files = from_dir(args.audio)
+        diarized_fragments = {}
+    else:
+        files = [args.audio, ]
+
+    file_iterator = tqdm(files)
+    for file in file_iterator:
+        file_iterator.set_description(f'"{file}" is processing now')
+        catalog_per_audio = os.path.splitext(os.path.basename(file))[0]
+        if len(files) > 1:
+            dir_for_parts = os.path.join(args.dir_save, catalog_per_audio)
+            if not os.path.exists(dir_for_parts):
+                os.makedirs(dir_for_parts)
+        else:
+            dir_for_parts = args.dir_save
+
+        if args.cuts_on:
+            parts, total_secs = cut_wav_on_parts(file, args.cuts_on)
+            piece = {}
+            iterator = tqdm(parts.keys(), leave=False)
+            for i, part in enumerate(iterator):
+                piece['name'] = part
+                piece['path_to_dir'] = dir_for_parts
+
+                iterator.set_description(f'{parts[part]["begin"]}-{parts[part]["end"]} sec (total: {total_secs}) processing')
+
+                diarized_fragments[file] = wav_proccecing(args, piece, start_from=i * args.cuts_on) # TYT
+                os.remove(part)
+
+        else:
+            diarized_fragments = wav_proccecing(args, file)
 
     return diarized_fragments
 
